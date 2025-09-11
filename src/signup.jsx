2e2aace24c37b79b53cfe24da118ff4f2978acb5
RevanthEshwar <2400032899@kluniversity.in>
2025-09-11 10:37:12 +0530

    message

---
+++ src/signup.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class signup extends Component {
+  render() {
+    return (
+     <div className="signup">
+      <h2>Create Your Account</h2>
+      {/* Add your signup form here */}
+    </div>
+    )
+  }
+}
